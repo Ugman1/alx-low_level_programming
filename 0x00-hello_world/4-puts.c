1f49b26810078be65c99a7601d0d75f28572c5f7
Ugman1 <ugochukwunwaorgu@gmail.com>
2022-06-30 16:47:54 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-/*
- * main - Entry point
- *  * Return: Always 0 (sucess)
+/**
+ *  *main - entry point
+ *   *
+ *    *Return: 0 after printing the function
 */
 int main(void)
 {
