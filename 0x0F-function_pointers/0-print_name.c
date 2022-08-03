442e3917f8723c0f37f980d1d28abafe42c77f8d
Ugman1 <ugochukwunwaorgu@gmail.com>
2022-08-03 16:26:00 -0700

    me

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,16 +1,13 @@
 #include "function_pointers.h"
 
 /**
- * print_name - a function that prints a name
- * by Ugochukwu Nwaorgu
- *
- * @name: recieved arguememt
- * @f: name of function to be called back
- * Return: void
+ * print_name - prints a name
+ * @name: string
+ * @f: function
  */
 
-void print_name(char *name, void (*f)(char *));
+void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
-		f(name);
+if (f != NULL)
+	f(name);
 }
