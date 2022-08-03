337eb6f2c971dbb02e9ab1aa81a48de4fa8b1f99
Ugman1 <ugochukwunwaorgu@gmail.com>
2022-08-03 14:56:52 -0700

    me

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,12 +4,13 @@
  * print_name - a function that prints a name
  * by Ugochukwu Nwaorgu
  *
- * name: recieved arguememt
- * f: name of function to be called back
+ * @name: recieved arguememt
+ * @f: name of function to be called back
  * Return: void
  */
 
 void print_name(char *name, void (*f)(*char));
 {
-	f(name)
+	if (name != NULL && f != NULL)
+	f(name);
 }
